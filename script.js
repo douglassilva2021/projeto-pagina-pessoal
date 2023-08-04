9923cd43297361c0fa5e9f2f78cfef2b0c7d3aba
douglassilva2021 <77202911+douglassilva2021@users.noreply.github.com>
2023-08-04 12:59:06 -0300

    Add files via upload
    Criando o arquivo script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnGreet").addEventListener("click", function() {
+    var greetingMessage = document.getElementById("greetingMessage");
+    greetingMessage.textContent = "Olá! Seja bem-vindo à minha página pessoal!";
+});
